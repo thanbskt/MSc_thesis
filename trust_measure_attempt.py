8cf79e651a716a48de29d35b1f45b3a8613037ed
thanbskt <athlyras@gmail.com>
2021-01-09 04:42:42 +0200

    trying to fix credibility equations

--- trust_measure_attempt.py
+++ trust_measure_attempt.py
@@ -17,7 +17,8 @@ dataset = pd.read_csv('justdoit_tweets_2018_09_07_2.csv',  parse_dates=['tweet_c
 pd.set_option('display.max_colwidth', 10000)
 pd.set_option('display.max_columns', None)
 
-dataset = dataset[[  'tweet_created_at',
+dataset = dataset[[  'user_id',
+                     'tweet_created_at',
                      'tweet_entities',
                      'tweet_full_text',
                      'tweet_id',
@@ -30,15 +31,21 @@ dataset = dataset[[  'tweet_created_at',
                      'user_followers_count',
                      'user_following',
                      'user_friends_count',
-                     'user_id',
                      'user_profile_background_color',
                      'user_verified']]
+#checking for miisng values
+sns.heatmap(dataset.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+#droping rows without missing values or selecting the non-missing ones
+dataset = dataset[dataset['user_id'].notna() ]
+#sorting our values based on user_id
+dataset = dataset.sort_values(by=['user_id']).reset_index(drop=True)
+
 #visualize the most common colors of backgraound profile
 colors = dataset['user_profile_background_color'].value_counts().index.values.tolist()
 color_counts = dataset['user_profile_background_color'].value_counts().values.tolist()
 colors = colors[:20]
 color_counts=color_counts[:20]
-
+        #
 for count,values in enumerate(colors):
     colors[count] = "#"+values
     print(count,values,"  ",colors[count])
@@ -53,58 +60,85 @@ tweet_dataset_5min.plot(figsize=(18,6))
 plt.ylabel('5 Minute Tweet Count')
 plt.title('Tweet Freq. Count')
 plt.grid(True)
+plt.show
 #visualize the retweets_count
 x= dataset['tweet_retweet_count'].index.values
 y=dataset['tweet_retweet_count'].values
 plt.scatter(x,y)
 plt.show()
 
+dataset = dataset[["user_id",
+                 'tweet_full_text',
+                 "tweet_id",
+                 'tweet_in_reply_to_user_id',
+                 "tweet_retweet_count",
+                 "user_favourites_count",
+                 "user_followers_count",
+                 "user_friends_count",
+                 "user_profile_background_color",
+                 "user_verified"]]
 
 
 
+
+
+
+
+#dataset['user_id'] = dataset['user_id'].astype('Int64')
+
+#we isolate the unique user by their id
+users_ids = dataset['user_id'].unique().tolist()
+users = pd.DataFrame(data = users_ids,columns = ['user_id']).sort_values(by=['user_id'])
+mean_values = dataset.groupby('user_id').mean().sort_index()
+#dataset['tweet_user']= dataset['tweet_user'].apply(literal_eval)
+
+users["RT_u"] = mean_values['tweet_retweet_count'].values
+users["Fo_u"] = mean_values['user_followers_count'].values
+users["Fe_u"] =  mean_values['user_friends_count'].values
+users['tweets_per_user_t'] = dataset['user_id'].value_counts().sort_index().values
+
+RT_mean = users["RT_u"].mean()
+Fo_mean = users['Fo_u'].mean()
+Fe_mean = users['Fe_u'].mean()
+
+
+
+CredRT = abs(RT_u - RT_mean)
+
+Utility = abs(RT_u*Fo_u/t_u - RT_mean*Fo_mean/t)
+
+Cred_social = abs(Fo_u/t_u - Fo_mean/t)
+
+Balance = (Fo_u/Fe - Fo_mean/Fe_mean)
+
+
+
+
+
+
+X = dataset [dataset['user_id'] ==  users[1]]
+testttt = dataset.set_index(['user_id'])
+
+test2=dataset.stack()
+dataset.groupby('user_id').cumcount()
+
+
+testttt.sort_index(inplace=True)
 
 dataset.nunique()
 
-dataset['tweet_user']= dataset['tweet_user'].apply(literal_eval)
-#afairoume tous xristes pou den exoun user_id
-dataset['user_id'].dropna(inplace=True)
-
-dataset['user_id'] = dataset['user_id'].astype('Int64')
-#we isolate the unique user by their id
-users = dataset['user_id'].unique().astype('Int64')
-
-X = dataset [dataset['user_id'] ==  users[0]]
-
-test = dataset[dataset['user_id'].isnull()]
 
 
 
 
 
-type(Y['id'])
-X
-Y = X[1249]
-keys = []
-values = []
-for k,d in X[1249]:
-	print(k,d)
-    
-Y['id']
-type(X)
-y = dataset['user_id'].unique()
-len(y)
-    
-type(dataset['tweet_created_at'][0])
 
 
 
 
-line1 = ax1.plot(x, myvalues,'#333399', marker='o', label='My Blue values')
 
 
 
-
-################TESTTTTT##################
 
 
 
