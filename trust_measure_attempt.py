377aa839c20bc4679e8f9535f8a93302b03ef5b5
thanbskt <athlyras@gmail.com>
2021-01-10 02:25:44 +0200

    construct users table with equations metrics and attempt ot measure final trust Cu

--- trust_measure_attempt.py
+++ trust_measure_attempt.py
@@ -64,6 +64,8 @@ plt.show
 #visualize the retweets_count
 x= dataset['tweet_retweet_count'].index.values
 y=dataset['tweet_retweet_count'].values
+plt.ylabel('number_of_retweets')
+plt.xlabel('tweet_id')
 plt.scatter(x,y)
 plt.show()
 
@@ -92,29 +94,55 @@ users = pd.DataFrame(data = users_ids,columns = ['user_id']).sort_values(by=['us
 mean_values = dataset.groupby('user_id').mean().sort_index()
 #dataset['tweet_user']= dataset['tweet_user'].apply(literal_eval)
 
+
+
+#We try to implement the equations in the paper: MODELING TOPIC SPECIFIC CREDIBILITY ON TWITTER
 users["RT_u"] = mean_values['tweet_retweet_count'].values
 users["Fo_u"] = mean_values['user_followers_count'].values
 users["Fe_u"] =  mean_values['user_friends_count'].values
-users['tweets_per_user_t'] = dataset['user_id'].value_counts().sort_index().values
+users['tweets_per_user_tu'] = dataset['user_id'].value_counts().sort_index().values
+user['verified'] = dataset['user_verified']
 
 RT_mean = users["RT_u"].mean()
 Fo_mean = users['Fo_u'].mean()
 Fe_mean = users['Fe_u'].mean()
+tx = t = len(dataset)
+#equation (1) CredRT(u,x) = |RTu - RTx_mean|
+
+#equation (2) Utility(u,x) = |RTu,x*Fo(u)/t(u,x) -RTx_mean * F(o,x)_mean/tx 
+
+#equation (3) Credsocial(u) = |Fo(u)/tu -Fo_mean/t_total | 
+
+#equation (4) Balance_social(u) = |Fo(u)/Fe(u)  -  Fo_mean/Fe_mean|
+
+#equation (5) Credsocial(u,x) = |Fo(u,x)/tu,x  - Fo,x_mean/tx|
+
+#equation (6) Focus(u,x)      = |Sum of all tu,x/ sum of all tu|
+#for just one topic we get:
+#RTx_mean -> RT_mean
+#RTu,x    -> RTu  
+#Fo,x     -> Fo
+#tu,x     ->tu (tweets of user u)
+#tx       ->t  (tweets in total)
+
+users["CredRT(u,x)"] = abs(users["RT_u"] - RT_mean )
+users["Utility(u,x)"] = abs(((users["RT_u"]*users["Fo_u"])/users['tweets_per_user_tu']) - ((RT_mean*Fo_mean)/tx) )
+users["Credsocial(u)"] = abs((users["Fo_u"] /users['tweets_per_user_tu']) - (Fo_mean/t))
+users["Balance_social(u)"] = abs((users["Fo_u"]/users["Fe_u"]) - (Fo_mean/Fe_mean))
+#because we are dealing with one topic 
+#equation (3) bocomes the same with equation (5)
+users["Credsocial(u,x)"] = abs(users["Fo_u"]/users['tweets_per_user_tu'] - Fo_mean/tx)
+# and equation 6 becomes 1
+users["Focus(u,x)"] = abs(users['tweets_per_user_tu'].sum()/users['tweets_per_user_tu'].sum())
 
 
+a=0.5
+b=0.3
+c= 0.5
 
-CredRT = abs(RT_u - RT_mean)
-
-Utility = abs(RT_u*Fo_u/t_u - RT_mean*Fo_mean/t)
-
-Cred_social = abs(Fo_u/t_u - Fo_mean/t)
-
-Balance = (Fo_u/Fe - Fo_mean/Fe_mean)
-
-
-
-
-
+users["Cu"] = a*(users["Focus(u,x)"] + b*(users["Balance_social(u)"] * users["Credsocial(u)"])) + c*(users["Utility(u,x)"]*users["CredRT(u,x)"])
+users[]
+test = dataset[dataset['user_id'] ==  3307430803]
 
 X = dataset [dataset['user_id'] ==  users[1]]
 testttt = dataset.set_index(['user_id'])
